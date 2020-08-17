9e592e5af77cdf8882052d80e9edc0f90340d2e4
songqingchuan <1778326311@qq.com>
2020-08-17 18:40:15 +0800

    部门页面调整

--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -82,7 +82,7 @@
       </el-table-column>
     </el-table>
 
-    <!-- 添加或修改部门对话框 -->
+    <!-- 添加或修改部门对话框  -->
     <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-row>
