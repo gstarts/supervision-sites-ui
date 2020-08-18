b6eb05ba72372314c19b4645400f626a6c0e6434
齐帅 <qishuai@qq.com>
2020-08-18 14:43:02 +0800

    新增 企业信息录入功能

---
+++ src/views/basis/info/enterprise.vue
@@ -0,0 +1,373 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" label-width="68px">
+      <el-form-item label="公司名称" prop="eName">
+        <el-input
+          v-model="queryParams.eName"
+          placeholder="请输入公司名称"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="公司性质" prop="eType">
+        <el-select v-model="queryParams.eType" placeholder="请选择公司性质" clearable size="small">
+          <el-option label="请选择字典生成" value="" />
+        </el-select>
+      </el-form-item>
+    
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['enterprise:info:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['enterprise:info:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['enterprise:info:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['enterprise:info:export']"
+        >导出</el-button>
+      </el-col>
+    </el-row>
+    <el-table v-loading="loading" :data="infoList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="序号"  tabindex="type" />
+      <el-table-column label="公司名称" align="center" prop="eName" />
+      <el-table-column label="公司性质" align="center" prop="eType" />
+      <el-table-column label="公司地址" align="center" prop="eAddress" />
+      <el-table-column label="法人" align="center" prop="eLegalPerson" />
+      <el-table-column label="营业范围" align="center" prop="eBusinessScope" />
+      <el-table-column label="备注信息" align="center" prop="remark" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['enterprise:info:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['enterprise:info:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>  
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改企业信息备案对话框 -->
+    <el-dialog :title="title" :visible.sync="open"  append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+        <el-row>
+          <el-col span="16">
+        <el-form-item label="公司名称" prop="eName">
+          <el-input v-model="form.eName" placeholder="请输入公司名称" />
+        </el-form-item>
+          </el-col>
+          <el-col span="8">
+        <el-form-item label="公司性质">
+          <el-select v-model="form.eType" placeholder="请选择公司性质">
+            <el-option label="请选择字典生成" value="" />
+          </el-select>
+        </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col span="12">
+        <el-form-item label="公司英文名称" prop="eEname">
+          <el-input v-model="form.eEname" placeholder="请输入公司英文名称" />
+        </el-form-item>
+          </el-col>
+          <el-col span="12">
+        <el-form-item label="公司简称" prop="eAbbreviation">
+          <el-input v-model="form.eAbbreviation" placeholder="请输入公司简称" />
+        </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item label="公司地址" prop="eAddress">
+          <el-input v-model="form.eAddress" placeholder="请输入公司地址" />
+        </el-form-item>
+        <el-form-item label="法人" prop="eLegalPerson">
+          <el-input v-model="form.eLegalPerson" placeholder="请输入法人" />
+        </el-form-item>
+        <el-form-item label="法人电话" prop="eLegalPersonPhone">
+          <el-input v-model="form.eLegalPersonPhone" placeholder="请输入法人电话" />
+        </el-form-item>
+        <el-form-item label="业务联系人" prop="eBusinessPerson">
+          <el-input v-model="form.eBusinessPerson" placeholder="请输入业务联系人" />
+        </el-form-item>
+        <el-form-item label="业务联系人电话" prop="eBusinessPersonPhone">
+          <el-input v-model="form.eBusinessPersonPhone" placeholder="请输入业务联系人电话" />
+        </el-form-item>
+        <el-form-item label="注册时间" prop="eRegisterTime">
+          <el-date-picker clearable size="small" style="width: 200px"
+            v-model="form.eRegisterTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="选择注册时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="营业范围" prop="eBusinessScope">
+          <el-input v-model="form.eBusinessScope" placeholder="请输入营业范围" />
+        </el-form-item>
+        <el-form-item label="海关编号" prop="customsMaster">
+          <el-input v-model="form.customsMaster" placeholder="请输入海关编号" />
+        </el-form-item>
+        <el-form-item label="组织机构代码" prop="contractorCode">
+          <el-input v-model="form.contractorCode" placeholder="请输入组织机构代码" />
+        </el-form-item>
+        <el-form-item label="统一社会信用代码" prop="contractorCodeScc">
+          <el-input v-model="form.contractorCodeScc" placeholder="请输入统一社会信用代码" />
+        </el-form-item>
+        <el-form-item label="作业场所编号" prop="supvLoctCode">
+          <el-input v-model="form.supvLoctCode" placeholder="请输入作业场所编号" />
+        </el-form-item>
+        <el-form-item label="操作人ID" prop="opUserId">
+          <el-input v-model="form.opUserId" placeholder="请输入操作人ID" />
+        </el-form-item>
+        <el-form-item label="发送ID" prop="senderId">
+          <el-input v-model="form.senderId" placeholder="请输入发送ID" />
+        </el-form-item>
+        <el-form-item label="接收ID" prop="receiverId">
+          <el-input v-model="form.receiverId" placeholder="请输入接收ID" />
+        </el-form-item>
+        <el-form-item label="接口协议版本号" prop="version">
+          <el-input v-model="form.version" placeholder="请输入接口协议版本号" />
+        </el-form-item>
+        <el-form-item label="备注信息" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入备注信息" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listInfo, getInfo, delInfo, addInfo, updateInfo } from "@/api/basis/enterpriseInfo";
+
+export default {
+  name: "Info",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 总条数
+      total: 0,
+      // 企业信息备案表格数据
+      infoList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 20,
+        eName: undefined,
+        eType: undefined,
+        eEname: undefined,
+        eAbbreviation: undefined,
+        eAddress: undefined,
+        eLegalPerson: undefined,
+        eLegalPersonPhone: undefined,
+        eBusinessPerson: undefined,
+        eBusinessPersonPhone: undefined,
+        eRegisterTime: undefined,
+        eBusinessScope: undefined,
+        customsMaster: undefined,
+        contractorCode: undefined,
+        contractorCodeScc: undefined,
+        supvLoctCode: undefined,
+        opUserId: undefined,
+        senderId: undefined,
+        receiverId: undefined,
+        version: undefined,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    /**获取企业类型 */
+     this.getDicts("station_enterprise_type").then(response => {
+      this.eEnterpriseTypeOptions = response.data;
+    });
+    this.getList();
+  },
+  methods: {
+    /** 查询企业信息备案列表 */
+    getList() {
+      this.loading = true;
+      listInfo(this.queryParams).then(response => {
+        this.infoList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: undefined,
+        eName: undefined,
+        eType: undefined,
+        eEname: undefined,
+        eAbbreviation: undefined,
+        eAddress: undefined,
+        eLegalPerson: undefined,
+        eLegalPersonPhone: undefined,
+        eBusinessPerson: undefined,
+        eBusinessPersonPhone: undefined,
+        eRegisterTime: undefined,
+        eBusinessScope: undefined,
+        customsMaster: undefined,
+        contractorCode: undefined,
+        contractorCodeScc: undefined,
+        supvLoctCode: undefined,
+        opUserId: undefined,
+        senderId: undefined,
+        receiverId: undefined,
+        version: undefined,
+        remark: undefined
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!=1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加企业信息备案";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getInfo(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改企业信息备案";
+      });
+    },
+    /** 提交按钮 */
+    submitForm: function() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != undefined) {
+            updateInfo(this.form).then(response => {
+              if (response.code === 200) {
+                this.msgSuccess("修改成功");
+                this.open = false;
+                this.getList();
+              }
+            });
+          } else {
+            addInfo(this.form).then(response => {
+              if (response.code === 200) {
+                this.msgSuccess("新增成功");
+                this.open = false;
+                this.getList();
+              }
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$confirm('是否确认删除企业信息备案编号为"' + ids + '"的数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return delInfo(ids);
+        }).then(() => {
+          this.getList();
+          this.msgSuccess("删除成功");
+        }).catch(function() {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('enterprise/info/export', {
+        ...this.queryParams
+      }, `enterprise_info.xlsx`)
+    }
+  }
+};
+</script>
