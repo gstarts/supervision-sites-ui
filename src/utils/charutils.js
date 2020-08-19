cf04a08bc183169416a65033779dfcddf03b70d8
meng <meng@ibrave.net>
2020-08-19 14:43:20 +0800

    增加工具类，通过传入参数 返回用户部门及所有下属部门 。

--- src/utils/charutils.js
+++ src/utils/charutils.js
@@ -1,3 +1,5 @@
+import store from '@/store/index'
+
 //生成26个英文字母
 export function genEnglishChar(){
     let wordArr = []
@@ -14,4 +16,25 @@ export function genNumChar(start,end){
         numArr.push({'key':i,'value': i<10?'0'+i:i})
     }
     return numArr
+}
+
+//获取用户所在部门及下属部门
+export function getUserDepts(deptType){
+    let depts = []
+    let dept = store.getters.dept
+    // 0 监管场所，1保税库，2堆场，3企业
+    //如果所属部门是堆场，加入列表
+    if(dept.deptType == deptType){
+        depts.add(dept)
+    }
+
+    if(dept.children && dept.children.length>0){
+        for(let item of dept.children){
+            if(item.deptType == deptType){
+                depts.push(item)
+            }
+        }
+    }
+
+    return depts;
 }
