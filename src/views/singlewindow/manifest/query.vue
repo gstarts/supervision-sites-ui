ec17a4cb7b7a77f8c0989d9bae59313929844a47
齐帅 <qishuai@qq.com>
2020-09-08 18:14:07 +0800

    feat:   公路舱单查询

--- src/views/singlewindow/manifest/query.vue
+++ src/views/singlewindow/manifest/query.vue
@@ -58,18 +58,18 @@
       :data="manifestList"
       @selection-change="handleSelectionChange"
     >
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="货物运输批次号" align="center" prop="declarationId" />
-      <el-table-column label="录入时间" align="center" prop="createTime" />
-      <el-table-column label="单证状态" align="center" prop="statementCode" :formatter="shipTypeFormat" />
-      <el-table-column label="单证名称" align="center" prop="messageType" :formatter="inOutMarkFormat" />
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="货物运输批次号" align="center" prop="declarationId"/>
+      <el-table-column label="录入时间" align="center" prop="createTime"/>
+      <el-table-column label="单证状态" align="center" prop="statementCode" :formatter="statementFormat"/>
+      <el-table-column label="单证名称" align="center" prop="messageType" :formatter="messageTypeFormat"/>
       <el-table-column
         label="报文功能"
         align="center"
         prop="functionCode"
         :formatter="viaVehicleFormat"
       />
-      <el-table-column label="回执说明" align="center" prop="statementDescription" />
+      <el-table-column label="回执说明" align="center" prop="statementDescription"/>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
@@ -77,21 +77,24 @@
             type="text"
             icon="el-icon-detail"
             @click="detail(scope.row)"
-          >详情</el-button>
+          >详情
+          </el-button>
           <el-button
             v-if="scope.row.statementCode=='10' || scope.row.statementCode=='20'|| scope.row.statementCode=='0'|| scope.row.statementCode=='FF'|| scope.row.statementCode=='3' "
             size="mini"
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-          >修改</el-button>
+          >修改
+          </el-button>
           <el-button
             v-if="scope.row.statementCode!='2'"
             size="mini"
             type="text"
             icon="el-icon-edit"
             @click="handleDelete(scope.row)"
-          >删除</el-button>
+          >删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -126,101 +129,104 @@ export default {
       // 总条数
       total: 0,
       // 舱单列表
-     manifestList: [],
+      manifestList: [],
+      // 单证状态
+      statementCodeOptions: [],
       // 弹出层标题
-      title: "",
+      title: '',
       // 是否显示弹出层
-      router:[
-        {path:'/rmft1401',messageType:'MT1401',value:'原始舱单'},
-        {path:'/rmft2401',messageType:'MT2401',value:'预配舱单'},
-        {path:'/rmft5402',messageType:'MT5402',value:'出口理货报告'},
-        {path:'/rmft3402',messageType:'MT3402',value:'运抵报告'},
-        {path:'/rmft4401',messageType:'MT4401',value:'载货进境确报'},
-        {path:'/rmft4403',messageType:'MT4403',value:'空载进境确报'},
-        {path:'/rmft4404',messageType:'MT4404',value:'空载出境确报'},
-        {path:'/rmft4406',messageType:'MT4406',value:'空箱出境确报'}],
+      router: [
+        { path: '/rmft1401', messageType: 'MT1401', value: '原始舱单' },
+        { path: '/rmft2401', messageType: 'MT2401', value: '预配舱单' },
+        { path: '/rmft5402', messageType: 'MT5402', value: '出口理货报告' },
+        { path: '/rmft3402', messageType: 'MT3402', value: '运抵报告' },
+        { path: '/rmft4401', messageType: 'MT4401', value: '载货进境确报' },
+        { path: '/rmft4403', messageType: 'MT4403', value: '空载进境确报' },
+        { path: '/rmft4404', messageType: 'MT4404', value: '空载出境确报' },
+        { path: '/rmft4406', messageType: 'MT4406', value: '空箱出境确报' }],
       open: false,
       // 查询参数
       queryParams: {
         pageNum: 1,
         pageSize: 10,
         statementCode: undefined,
-        declarationId:undefined,
-        messageType:undefined,
-      },
-    };
+        declarationId: undefined,
+        messageType: undefined
+      }
+    }
   },
   mounted() {
-      this.getList();
+    this.getList()
+    /** 单证状态 */
+    this.getDicts("station_declear_status").then(response => {
+      this.statementCodeOptions = response.data;
+    });
   },
   methods: {
     /** 查询公路舱单列表 */
     getList() {
       manifestList(this.addDateRange(this.queryParams, this.dateRange)).then(
         response => {
-          this.manifestList = response.rows;
-          this.total = response.total;
-          this.loading = false;
+          this.manifestList = response.rows
+          this.total = response.total
+          this.loading = false
         }
-      );
+      )
     },
-    // 改变驳回数据颜色
-    // 运输方式翻译
 
     // 单证状态翻译
-
+    statementFormat(row) {
+      return this.selectDictLabel(this.statementCodeOptions, row.statementCode);
+    },
     // 单证名称翻译
-
+    messageTypeFormat(row) {
+      const data = this.router.find(el => el.messageType === row.messageType)
+      return data.value
+    },
     // 报文功能翻译
 
     // 取消按钮
     cancel() {
-      this.open = false;
-      this.reset();
+      this.open = false
+      this.reset()
     },
 
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
-      this.getList();
+      this.queryParams.pageNum = 1
+      this.getList()
     },
     /** 重置按钮操作 */
     resetQuery() {
-      this.dateRange = [];
-      this.resetForm("queryForm");
-      this.handleQuery();
+      this.dateRange = []
+      this.resetForm('queryForm')
+      this.handleQuery()
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id);
-      this.single = selection.length != 1;
-      this.multiple = !selection.length;
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length != 1
+      this.multiple = !selection.length
     },
 
     /** 修改按钮操作 */
     handleUpdate(row) {
-      debugger
-      const id = row.id || this.ids;
+      const id = row.id || this.ids
       // 跳转到原始舱单页面
-      const data= this.router.find(el => el.messageType === row.messageType)
-      this.$router.push({ path: "/singlewindow"+data.path });
-    },
-
-    /** 打开新增弹窗 */
-    openAddDialog() {
-      this.$refs.import.show();
+      const data = this.router.find(el => el.messageType === row.messageType)
+      this.$router.push({ path: '/singlewindow' + data.path })
     },
 
     /**详情按钮 */
     detail(row) {
       // this.reset();
-      return this.$message("功能正在完善中...")
-      const id = row.id || this.ids;
-      const data= this.router.find(el => el.messageType === row.messageType)
-      this.$router.push({ path: "/singlewindow"+data.path });
+      return this.$message('功能正在完善中...')
+      const id = row.id || this.ids
+      const data = this.router.find(el => el.messageType === row.messageType)
+      this.$router.push({ path: '/singlewindow' + data.path })
     }
   }
-};
+}
 </script>
 <style>
 
