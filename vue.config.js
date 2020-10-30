b00edb8348f6eb0e5d99d094d14f084fcefc2afb
齐帅 <qishuai@qq.com>
2020-10-30 23:46:48 +0800

    feat:    配置文件链接到本地

--- vue.config.js
+++ vue.config.js
@@ -35,9 +35,9 @@ module.exports = {
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://localhost:9090`,
         // 本地
-	      // target: `http://192.168.11.199:9090`,
+	      target: `http://192.168.11.199:9090`,
         // 华方
-	      target: `http://10.0.130.51:9090`,
+	      // target: `http://10.0.130.51:9090`,
 	      // target: `http://39.105.36.197:9090`,
         changeOrigin: true,
         pathRewrite: {
