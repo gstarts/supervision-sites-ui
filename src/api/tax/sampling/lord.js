2fcf14e150668d04e89c20a96722bbe1b0913d79
蓝振原 <lanzhenyuan@qq.com>
2020-12-16 18:20:41 +0800

    取样管理

---
+++ src/api/tax/sampling/lord.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询取样管理 主列表
+export function listLord(query) {
+  return request({
+    url: '/tax/lord/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询取样管理 主详细
+export function getLord(id) {
+  return request({
+    url: '/tax/lord/' + id,
+    method: 'get'
+  })
+}
+
+// 新增取样管理 主
+export function addLord(data) {
+  return request({
+    url: '/tax/lord',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改取样管理 主
+export function updateLord(data) {
+  return request({
+    url: '/tax/lord',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除取样管理 主
+export function delLord(id) {
+  return request({
+    url: '/tax/lord/' + id,
+    method: 'delete'
+  })
+}
