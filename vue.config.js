75b28518032f2ba06b625fb4ea6d4317db7459bd
Livon <55000151@qq.com>
2021-02-14 11:05:21 +0800

    远程 VPN 环境开发

--- vue.config.js
+++ vue.config.js
@@ -33,9 +33,10 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        // target: `http://localhost:9090`,
+        // target: `http://192.168.11.199:9090`,
+        target: `http://169.254.154.82:9090`,
 	      // target: `http://10.0.130.51:9090`,
-	      target: `http://39.105.36.197:9090`,
+	      // target: `http://39.105.36.197:9090`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
